5081f76ac3c9daa393e3488672b1a6aa3a968a83
Will Turner <willguyturner@gmail.com>
2025-03-08 11:17:36 +1300

    Add image support for posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,7 +112,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# Media files (.png)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
